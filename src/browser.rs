984b890cfd3d99e95c29afa3b7a3017a8862cf64
AutomatedTester <david.burns@theautomatedtester.co.uk>
2020-08-02 22:11:11 +0100

    Add the ability to create new browser structs

--- src/browser.rs
+++ src/browser.rs
@@ -1,5 +1,28 @@
 struct Browser {
-    name: &'static str,
-    driver_path: &'static str,
-    browser_path: &'static str,
+    name: String,
+    driver_path: String,
+    browser_path: String,
+}
+
+impl Browser {
+    fn new(name: String, driver_path: String, browser_path: String) -> Self {
+        Self { name: name,
+               driver_path: driver_path,
+               browser_path:browser_path
+            }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_browser() {
+        let firefox = Browser::new(String::from("firefox"), String::from("driver_path"), String::from("browser_path"));
+        assert_eq!(firefox.name, String::from("firefox"));
+        assert_eq!(firefox.driver_path, String::from("driver_path"));
+        assert_eq!(firefox.browser_path, String::from("browser_path"));
+    }
+
 }
